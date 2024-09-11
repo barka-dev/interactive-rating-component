81d3a9c4463c41dc6c28f5fffc7bda28fe74a337
Abdel <abdelbarka96@gmail.com>
2024-09-11 14:01:51 +0100

    final version

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-const main_el = document.querySelector("main");
+const labels = document.querySelectorAll(".labels");
+const radios = document.querySelectorAll("input[type='radio']");
+const form = document.querySelector("form");
+const result_container = document.querySelector(".result_container");
+const rate_container = document.querySelector(".rate_container");
+const selected_rate = document.querySelector(".selected_rate");
 
-const isResult = true;
+const remove_selection = ()=>{
+    labels.forEach((label)=>{
+        label.classList.remove('active');
+    });
+}
 
-// if(isResult){
-//     main_el.innerHTML = `<rate-component></rate-component>`
-// }else{
-//     main_el.innerHTML = `<result-component></result-component>`
-// }
+radios.forEach((radio)=>{
+    radio.addEventListener('change',(e)=>{
+        remove_selection();
+        e.target.parentElement.classList.add("active");
+    })
+})
+
+const getSelectedRate = ()=>{
+    const selected = document.querySelector("input[type='radio']:checked").value;
+    return selected;
+}
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    selected_rate.textContent = getSelectedRate();
+    rate_container.style.display = 'none';
+    result_container.style.display = 'flex';
+})
